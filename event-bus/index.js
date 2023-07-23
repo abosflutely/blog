f7eb546bb8883957e2fd9817e0e6af1a593b433b
abosflutely <achen315@student.fuhsd.org>
2023-07-23 10:20:06 -0700

    change urls

--- event-bus/index.js
+++ event-bus/index.js
@@ -16,18 +16,18 @@ app.post('/events', (req, res) => {
         console.log(err.message);
     }
     );
-    // axios.post('http://localhost:4001/events', event).catch((err) =>
-    // {
-    //     console.log(err.message);
-    // });
-    // axios.post('http://localhost:4002/events', event).catch((err =>{
-    //     console.log(err.message);
-    // }
-    // ));
-    // axios.post('http://localhost:4003/events', event).catch((err =>{
-    //     console.log(err.message);
-    // }
-    // ));
+    axios.post('http://comments-srv:4001/events', event).catch((err) =>
+    {
+        console.log(err.message);
+    });
+    axios.post('http://query-srv:4002/events', event).catch((err =>{
+        console.log(err.message);
+    }
+    ));
+    axios.post('http://moderation-srv:4003/events', event).catch((err =>{
+        console.log(err.message);
+    }
+    ));
 
     app.get('/events', (req, res) => {
         res.send(events);
